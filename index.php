fd71bcf7903341628b027122ef8e7d122c86eb14
Wenesley <wenesleysantos@gmail.com>
2020-05-01 00:15:25 -0300

    tpl site

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ $app->get('/', function() {
 	$page = new Page(); //Nesse momento de criação da página, o construtor é acionado e cria o header.html.
 
 	$page->setTpl("index"); //quando chamamos o metodo setTpl e passamos o template, nesse caso ele chama a página index.html e adiciona o corpo da página. quando seguir para próxima linha após a chamda encerra a execução, ou seja limpa a memória do seu site, e automaticamente o método destruct é acionado, o arquivo footer.html e inserido no template.
-
-
-
 });
 
 $app->run(); //Tudo carregado, vamos executar.
